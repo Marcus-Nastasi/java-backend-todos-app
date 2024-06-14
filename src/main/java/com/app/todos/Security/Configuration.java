2a4cf42defd666cc737614e054ab19d7ea0ba2a8
rolemberg <vinnie.nsts@gmail.com>
2024-06-14 12:12:36 -0300

    created Security Config and SecurityService

---
+++ src/main/java/com/app/todos/Security/Configuration.java
@@ -0,0 +1,25 @@
+package com.app.todos.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+
+@EnableWebSecurity
+public class Configuration extends DelegatingWebMvcConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return(
+            httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                    .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .build()
+        );
+    }
+}
+
+
+
