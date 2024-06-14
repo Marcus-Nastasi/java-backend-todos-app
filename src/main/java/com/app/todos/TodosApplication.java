a99f804869f536aafb44d08cd0f1f0b31ff00f3e
rolemberg <vinnie.nsts@gmail.com>
2024-06-14 10:45:41 -0300

    first commit

---
+++ src/main/java/com/app/todos/TodosApplication.java
@@ -0,0 +1,13 @@
+package com.app.todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosApplication.class, args);
+	}
+
+}
