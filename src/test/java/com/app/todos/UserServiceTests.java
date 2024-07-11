69cec6b3a974e5c774aa184aaf050f8d99f263fb
rolemberg <vinnie.nsts@gmail.com>
2024-07-11 00:33:23 -0300

    implementing get user and new user testing

--- src/test/java/com/app/todos/UserServiceTests.java
+++ src/test/java/com/app/todos/UserServiceTests.java
@@ -1,22 +1,61 @@
 package com.app.todos;
 
+import com.app.todos.DTOs.Users.NewUserDTO;
+import com.app.todos.Models.Users.User;
+import com.app.todos.Repository.User.UserRepo;
+import com.app.todos.Services.Auth.TokenService;
 import com.app.todos.Services.Users.UserService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigInteger;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
 
-    @Autowired
+    @Mock
+    private UserRepo userRepo;
+    @Mock
+    private TokenService tokenService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
     private UserService userService;
 
     @Test
     void getUser() {
+        User user = new User("Brian", "brian@gmail.com", "12345");
+        String token = "token";
 
+        when(userRepo.findById(any(BigInteger.class))).thenReturn(Optional.of(user));
+        when(tokenService.validate(token)).thenReturn(user.getEmail());
+
+        assertDoesNotThrow(() -> {
+            userService.get(BigInteger.valueOf(2000), token);
+        });
+        assertEquals(userService.get(BigInteger.valueOf(2000), token), user);
+    }
+
+    @Test
+    void newUserTest() {
+        User user = new User("Brian", "brian@gmail.com", "12345");
+        NewUserDTO newUserDTO = new NewUserDTO("Brian", "brian@gmail.com", "12345");
+
+        when(userRepo.save(any(User.class))).thenReturn(user);
+
+        assertDoesNotThrow(() -> {
+            userService.newUser(newUserDTO);
+        });
     }
 }
 
