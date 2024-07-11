e709d6ca812f422458bde2859edf3ebc589d22d5
rolemberg <vinnie.nsts@gmail.com>
2024-07-11 10:36:09 -0300

    testing get todo method

--- src/test/java/com/app/todos/TodosServiceTests.java
+++ src/test/java/com/app/todos/TodosServiceTests.java
@@ -1,19 +1,67 @@
 package com.app.todos;
 
+import com.app.todos.Enums.Todos.Priority;
+import com.app.todos.Models.Todos.Todo;
+import com.app.todos.Models.Users.User;
+import com.app.todos.Repository.Todos.TodosRepo;
+import com.app.todos.Repository.User.UserRepo;
+import com.app.todos.Services.Auth.TokenService;
 import com.app.todos.Services.Todos.TodosService;
+import com.app.todos.Services.Users.UserService;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class TodosServiceTests {
 
-    @Autowired
+    @Mock
+    private TodosRepo todosRepo;
+    @Mock
+    private UserService userService;
+    @Mock
+    private UserRepo userRepo;
+    @Mock
+    private TokenService tokenService;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @InjectMocks
     private TodosService todosService;
 
     @Test
-    void testing() {
+    void getTodo() {
+        Todo todo = new Todo(BigInteger.valueOf(1500), "Coca-Cola", "Make machine", "Make refri machine", "none", LocalDate.of(2024, 07, 15), Priority.HIGH);
+        User user = new User("Brian", "bian@gmail.com", "12345");
+        String token = "Token";
+        String falseToken = "Token False";
 
+        when(todosRepo.findById(any(BigInteger.class))).thenReturn(Optional.of(todo));
+        when(userRepo.findById(any(BigInteger.class))).thenReturn(Optional.of(user));
+        when(tokenService.validate(token)).thenReturn(user.getEmail());
+
+        assertDoesNotThrow(() -> {
+            todosService.get(BigInteger.valueOf(1500), token);
+        });
+        assertEquals(todo, todosService.get(BigInteger.valueOf(1500), token));
+
+        when(tokenService.validate(falseToken)).thenThrow(JWTVerificationException.class);
+
+        assertThrows(JWTVerificationException.class, () -> {
+            todosService.get(BigInteger.valueOf(1500), falseToken);
+        });
     }
 }
 
